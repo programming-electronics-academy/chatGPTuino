caddc8861c6c40d8e0f1af6eb7634fcb8857b193
Michael Cheich <mcheich@gmail.com>
2023-06-26 11:55:31 -0400

    refactor: use environment variable for key instead of fake key

--- chatGPTuino_turbo/credentials.h
+++ chatGPTuino_turbo/credentials.h
@@ -4,6 +4,6 @@
 /* Keep all these private */
 const char* ssid = "Under_Ground_Layer";  // Change to your WiFi Network name
 const char* password = "mwahahaha";  // Change to your password
-const char* openAI_Private_key = "abc123-jackson5"; // your private OpenAi API Key
+const char* openAI_Private_key = "<yourkey>"; // your private OpenAi API Key
 
 #endif
