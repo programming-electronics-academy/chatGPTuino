2f72ff7454e31238fb82cbd622afa469ace8c939
programmingelectronics <michael@programmingelectronics.com>
2024-07-11 12:20:48 -0400

    fix:update root cert for api.openai.com

--- chatGPTuino_turbo/credentials.h
+++ chatGPTuino_turbo/credentials.h
@@ -6,4 +6,24 @@ const char* ssid = "Under_Ground_Layer";  // Change to your WiFi Network name
 const char* password = "mwahahaha";  // Change to your password
 const char* openAI_Private_key = "<yourkey>"; // your private OpenAi API Key
 
+/* OpenAI API endpoint root certificate used to ensure response is actually from OpenAPI
+
+  TROUBLESHOOTING: If you get the "Could not connect to server. Trying again."
+  message in the serial monitor, you may want to check that this root certificate below has not expired.
+*/
+const char* rootCACertificate =
+  "-----BEGIN CERTIFICATE-----\n"
+  "MIICCTCCAY6gAwIBAgINAgPlwGjvYxqccpBQUjAKBggqhkjOPQQDAzBHMQswCQYD\n"
+  "VQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2VzIExMQzEUMBIG\n"
+  "A1UEAxMLR1RTIFJvb3QgUjQwHhcNMTYwNjIyMDAwMDAwWhcNMzYwNjIyMDAwMDAw\n"
+  "WjBHMQswCQYDVQQGEwJVUzEiMCAGA1UEChMZR29vZ2xlIFRydXN0IFNlcnZpY2Vz\n"
+  "IExMQzEUMBIGA1UEAxMLR1RTIFJvb3QgUjQwdjAQBgcqhkjOPQIBBgUrgQQAIgNi\n"
+  "AATzdHOnaItgrkO4NcWBMHtLSZ37wWHO5t5GvWvVYRg1rkDdc/eJkTBa6zzuhXyi\n"
+  "QHY7qca4R9gq55KRanPpsXI5nymfopjTX15YhmUPoYRlBtHci8nHc8iMai/lxKvR\n"
+  "HYqjQjBAMA4GA1UdDwEB/wQEAwIBhjAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQW\n"
+  "BBSATNbrdP9JNqPV2Py1PsVq8JQdjDAKBggqhkjOPQQDAwNpADBmAjEA6ED/g94D\n"
+  "9J+uHXqnLrmvT/aDHQ4thQEd0dlq7A/Cr8deVl5c1RxYIigL9zC2L7F8AjEA8GE8\n"
+  "p/SgguMh1YQdc4acLa/KNJvxn7kjNuK8YAOdgLOaVsjh4rsUecrNIdSUtUlD\n"
+  "-----END CERTIFICATE-----\n";
+
 #endif
